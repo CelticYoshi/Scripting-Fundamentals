a12ec214990d5507d98bce6b100f0091471909fd
Drake B <drakeboard719@riversideunified.org>
2023-10-06 13:36:12 -0700

    Creating a scripting review script and updating files

---
+++ Scripting Examples/Assets/Scripts/ScriptingReview.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptingReview : MonoBehaviour
+{
+    public string FirstName = "Drake";
+    public int GradeLevel = 10;
+    public float GPA = 4.0f;
+    public bool CanYouUseNotesOnTheQuiz = true;
+
+    private int FavoriteNumber = 70942;
+    private string FavoriteMovie = "A Quiet Place";
+    private float PriceOfGas = 5.87f;
+    private bool WasTheReviewHelpful = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
